0f9caefea5f2288fc541a02f81c2a8b94166633a
Harsha <sriharshakaredla@gmail.com>
2026-03-11 12:11:08 +0530

    some

--- app.py
+++ app.py
@@ -4,10 +4,10 @@ from src.prediction import model_preds
 st.title("Some Model Prediction")
 st.write("This is some model")
 
-age = st.number_input("Enter your age : ")
-annual_income_lpa = st.number_input("Enter ur annual somehthing : ")
-policy_term_year = st.number_input("Enter policy")
-Sum_Assured_Lakhs = st.number_input("Enter somthing high val : ")
+age = st.number_input("Enter your Age : ")
+annual_income_lpa = st.number_input("Enter ur annual income : ")
+policy_term_year = st.number_input("Enter policy in yr")
+Sum_Assured_Lakhs = st.number_input("assured in lks : ")
 
 if st.button("predict"):
     model = model_preds()
